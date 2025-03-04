95c1aba18782106cd725299a4559e8a70728a010
Morozov Kirill <k.morozov@magdv.com>
2025-03-04 19:46:32 +0300

    Обновленный junit и Selenide+первый автотест на простую форму

---
+++ src/test/java/TestBoxTest.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBoxTest{
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.timeout = 60000;
+    }
+
+    @Test
+    void firstTest() {
+        open("/text-box");
+        $("#userName").setValue("Ivan");
+        $("#userEmail").setValue("Petrov_Ivan@Mail.ru");
+        $("#currentAddress").setValue("Russia, Moscow");
+        $("#permanentAddress").setValue("Russia, Moscow");
+        $("#submit").click();
+    }
+
+}
