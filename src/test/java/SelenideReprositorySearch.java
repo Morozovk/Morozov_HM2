1ab5fbabdb09b12f3d885c73096b7df5022c1570
Morozov Kirill <k.morozov@magdv.com>
2025-03-11 23:10:12 +0300

    Добавлен новый тест на сайт github

---
+++ src/test/java/SelenideReprositorySearch.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideReprositorySearch {
+    @BeforeAll
+    static void beforeAll (){
+        Configuration.browserSize = "1920x1080";
+        sleep(10000);
+    }
+
+    @Test
+    void test() {
+        open("https://github.com");
+        $(".search-input").$(".flex-1").click();
+        $("#query-builder-test").setValue("Selenide").pressEnter();
+        $(".search-match").click();
+        $$("ul li").findBy(text("Wiki")).click();
+        $$("ul li").findBy(text("Soft assertions")).click();
+    }
+}
