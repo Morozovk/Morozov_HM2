f54f9e38775dd7eeb37ba09482fb8c24116fdee0
Morozov Kirill <k.morozov@magdv.com>
2025-03-13 22:42:10 +0300

    автотест на репрозиторий selenide

--- src/test/java/SelenideReprositorySearch.java
+++ src/test/java/SelenideReprositorySearch.java
@@ -3,23 +3,25 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SelenideReprositorySearch {
     @BeforeAll
-    static void beforeAll (){
+    static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         sleep(10000);
     }
 
     @Test
     void test() {
-        open("https://github.com");
-        $(".search-input").$(".flex-1").click();
-        $("#query-builder-test").setValue("Selenide").pressEnter();
-        $(".search-match").click();
-        $$("ul li").findBy(text("Wiki")).click();
-        $$("ul li").findBy(text("Soft assertions")).shouldHave(text("Soft assertions"));
-        $$("ul li").findBy(text("Soft assertions")).click();
+        open("https://github.com/selenide/selenide");
+        element("a[id='wiki-tab']").click();
+        element("ul[data-filterable-for='wiki-pages-filter']").find(byCssSelector("button[type='button']")).click();
+        element("a[href='/selenide/selenide/wiki/SoftAssertions']").shouldBe(visible).click();
+        element("a[id='user-content-3-using-junit5-extend-test-class']").scrollTo().hover().shouldBe(visible);
+        element(byText("3. Using JUnit5 extend test class:")).shouldBe(visible);
     }
 }
