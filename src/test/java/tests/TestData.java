727b19d68b30ccd9fe81652c65abbcf3ea241d6d
Morozov Kirill <k.morozov@magdv.com>
2025-04-03 15:36:00 +0300

    сделал методы с генерацией тестовых данных

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,29 +1,61 @@
 package tests;
 
 import com.github.javafaker.Faker;
+import org.openqa.selenium.devtools.v132.css.model.Value;
 
 import java.util.Locale;
 
 public class TestData {
     private final Faker faker = new Faker((new Locale("en-GB")));
 
-    public String
-        firstName = faker.name().firstName(),
-        lastName = faker.name().lastName(),
-        email = faker.internet().emailAddress(),
-        notCorrectEmail = "123qwe",
-        gender = faker.options().option("Male" , "Female" , "Other"),
-        number = faker.number().digits(10),
-        monthBirth = faker.options().option("January", "February", "March", "April", "May", "June", "July",
-                "August", "September", "October", "November", "December"),
-        hobbies = faker.options().option("Sports" , "Reading" , "Music"),
-        currentAddress = faker.address().fullAddress(),
-        state = faker.options().option("NCR" , "Uttar Pradesh" , "Haryana" , "Rajasthan"),
-        city = selectCity(state);
+    public String firstName() {
+        return faker.name().firstName();
+    }
 
+    public String lastName() {
+        return faker.name().lastName();
+    }
 
-    public String yearBirth = String.valueOf(faker.number().numberBetween(1950,2025));
-    public String dayBirth = String.format("%02d", faker.number().numberBetween(1,28));
+    public String email() {
+        return faker.internet().emailAddress();
+    }
+
+    public String notCorrectEmail = "123qwe";
+
+    public String gender() {
+        return faker.options().option("Male" , "Female" , "Other");
+    }
+
+    public String number() {
+        return faker.number().digits(10);
+    }
+
+    public String monthBirth() {
+        return faker.options().option("January", "February", "March", "April", "May", "June", "July",
+                "August", "September", "October", "November", "December");
+    }
+
+    public String hobbies() {
+        return faker.options().option("Sports" , "Reading" , "Music");
+    }
+
+    public String currentAddress() {
+        return faker.address().fullAddress();
+    }
+
+    public String state() {
+        return faker.options().option("NCR" , "Uttar Pradesh" , "Haryana" , "Rajasthan");
+    }
+
+    public String city = selectCity(state());
+
+    public String yearBirth() {
+        return String.valueOf(faker.number().numberBetween(1950,2025));
+    }
+
+    public String dayBirth() {
+        return String.format("%02d", faker.number().numberBetween(1,28));
+    }
 
 
     public String selectCity (String state) {
@@ -36,7 +68,7 @@ public class TestData {
         };
     }
 
-    public String dayOfBirthday = dayBirth + " " + monthBirth + "," + yearBirth,
-    stateAndCity = state + " " + city,
-    fullName = firstName + " " + lastName;
+    public String dayOfBirthday = dayBirth() + " " + monthBirth() + "," + yearBirth(),
+    stateAndCity = state() + " " + city,
+    fullName = firstName() + " " + lastName();
 }
