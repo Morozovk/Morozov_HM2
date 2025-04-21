737c2b1234dae083c5b490183ed788234cc2df0e
Morozov Kirill <k.morozov@magdv.com>
2025-04-22 02:27:09 +0300

    Выбираем браузер и набор тестов в jenkins

--- src/test/java/tests/workWithJenkins/WorkWithJenkins.java
+++ src/test/java/tests/workWithJenkins/WorkWithJenkins.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.PracticeFormPage;
 import utils.TestData;
+
 import java.util.Map;
 
 import static io.qameta.allure.Allure.step;
